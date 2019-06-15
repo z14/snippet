fb3bcf536d18dfd018341a08a481b673c3f99337
Arc Zee <z@arcz.ee>
2019-06-15 20:40:22 +0800

    nth

--- cmd/del_before_date.cmd
+++ cmd/del_before_date.cmd
@@ -1,7 +1,9 @@
 @echo off
 
-REM Delete file with specified .ext and modified after specified date
-REt /b %errorlevel% Move this script to directory where files needed to be delete
+REM Delete file with specified .ext and modified before specified date
+REM For XP without forfiles.exe
+REM Move this script to directory where files needed to be delete
+
 set tmpdir=%RANDOM%%RANDOM%
 set date=01-01-2017
 set ext=log
