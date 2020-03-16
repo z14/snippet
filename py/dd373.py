31dccd7a26852383f9c0924f6d2b0c7dc713ef79
Arc Zee <z@arcz.ee>
2020-03-16 14:04:36 +0800

    nth

--- py/dd373.py
+++ py/dd373.py
@@ -2,18 +2,39 @@
 
 from bs4 import BeautifulSoup
 import requests
+import pymysql.cursors
 
 MIN_PRICE = 600
 
+source = 'dd373'
 url = 'https://dl.dd373.com/need/eja7u2-0-0-0-0-0-0-0-0-0-0.html'
 headers = {'user-agent': 'fuck/0.0.1'}
 html = requests.get(url, headers = headers).text
 soup = BeautifulSoup(html, 'html.parser')
-# print(soup)
+
+conn = pymysql.connect(
+        host='localhost',
+        user='wowjob',
+        password='wowjob',
+        db='wowjob',
+        cursorclass=pymysql.cursors.DictCursor
+        )
+cursor = conn.cursor()
+
+sql = f"select * from job where source = '{source}' limit 1"
+cursor.execute(sql)
+res = cursor.fetchone()
+if res:
+    last = res['gid']
+else:
+    last = 0
 
 glist = soup.select('.gl_content')
 
 for i in glist:
+    gid =  i.select_one('.goods_t .title a')['href'].lstrip('/NeedDetail-').rstrip('.html')
+    if gid == last:
+        break
     price = i.select_one('.goods_os').text.strip().lstrip('￥')
     if float(price) < MIN_PRICE:
         continue
@@ -23,11 +44,18 @@ for i in glist:
     days = i.select_one('.goods_time').text.strip()
 
     title = i.select_one('.goods_t .title a').text.strip()
-    gid =  i.select_one('.goods_t .title a')['href'].lstrip('/NeedDetail-').rstrip('.html')
     realmSum = i.select_one('.goods_t .mrgl7').find('div', attrs={'class': None}).select('a')
-    realm = realmSum[1].text +  '\t' + realmSum[2].text + '\t' + realmSum[3].text
+    zone = realmSum[1].text
+    realm = realmSum[2].text
+    faction = realmSum[3].text
 
-    # write gid  to database;
-    # print(gid)
+    # database;
+    sql = f"insert into job (gid, price, deposit, days, title, zone, realm, faction, source) values ('{gid}', '{price}', '{deposit}', '{days}', '{title}', '{zone}', '{realm}','{faction}', '{source}')"
+    cursor.execute(sql)
+    conn.commit()
 
-    print(int(float(price)),'\t', int(deposit), '\t', days, '\t', realm, '\t', title)
+    # msg = int(float(price)),'\t', int(deposit), '\t', days, '\t', zone, '\t', realm, '\t', faction, '\t', title
+    
+    # some api
+
+    print(int(float(price)),'\t', int(deposit), '\t', days, '\t', zone, '\t', realm, '\t', faction, '\t', title)
